aa182dab9142bb890156601affc58c264bf65660
Unknown <alkotobnour@gmail.com>
2018-04-18 02:44:41 +0200

    Syntax choice

--- produce.py
+++ produce.py
@@ -5,11 +5,10 @@ from shutil import move
 from subprocess import run
 start = time.time()
 magick = "" if os.name != "nt" else "magick"
-print("magick: " + magick)
 for f in glob.glob("./*[0-9].kra"):
     baseName = (splitext(basename(f))[0])
     zipfile.ZipFile(f, "r").extract("mergedimage.png")
-    run([magick, "convert", "-resize", "800", "-density", "300", "mergedimage.png", "../jpg/" + baseName + ".jpg"], shell=True)
-    #call(magick + "convert -resize 800 -density 300 mergedimage.png ../jpg/" + baseName + ".jpg")
+    #run([magick, "convert", "-resize", "800", "-density", "300", "mergedimage.png", "../jpg/" + baseName + ".jpg"], shell=True)
+    run(magick + "convert -resize 800 -density 300 mergedimage.png ../jpg/" + baseName + ".jpg", shell=True)
     move("mergedimage.png", "../png/" + baseName + ".png")
 print("time taken: %.2f " % (time.time() - start))
