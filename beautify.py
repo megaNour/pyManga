4b318c16eac8c402fe76cb60c706b9cb13efac40
Unknown <alkotobnour@gmail.com>
2018-06-04 09:27:34 +0200

    cutting manager dependency

--- beautify.py
+++ beautify.py
@@ -8,22 +8,26 @@ import argparse
 from manager import Manager
 from pathlib import Path
 
-def rename(newNamePrefix: str, start: int, step: int, patternStr: str):
+def rename(swapNamePrefix: str, start: int, step: int, patternStr: str, newNamePrefix: str):
+    #start becomes the index
     targets = glob.glob("*")
     targets.sort()
     pattern = re.compile(patternStr)
     for path in (path for path in targets if os.path.isfile(path)):
         matcher = pattern.search(path)
         if matcher:        
-            newPath = newNamePrefix + manager.getPageName(start, matcher.group(2))
+            newPrefix = newNamePrefix
+            if newNamePrefix == "managerInfered":
+                newPrefix = manager.getPageName(start, matcher.group(2))                
+            newPath = swapNamePrefix + newPrefix
             shutil.move(path, newPath)
         else :
             print(path + " doesn't comply to the patern")
         start += step
 
-def beautify(newNamePrefix: str = "", start: int = 1, step: int = 1, pattern: str = constants.INDEXED_FILENAME_PATTERN):
+def beautify(swapNamePrefix: str = "", start: int = 1, step: int = 1, pattern: str = constants.INDEXED_FILENAME_PATTERN, newNamePrefix="managerInfered"):
     rename("beautiful_", start, step, pattern)
-    rename(newNamePrefix, start, step, pattern)
+    rename(swapNamePrefix, start, step, pattern)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--pattern", nargs="?", help="pattern to target")
@@ -32,11 +36,11 @@ parser.add_argument("--step", nargs="?", help="increment step for renamed files"
 
 args, unknown = parser.parse_known_args()
 
-newNamePrefix = args.pattern if args.pattern else ""
+newNamePrefix = args.pattern if args.pattern else "managerInfered"
 start = int(args.start) if args.start else 1
 step = int(args.step) if args.step else 1
 
 manager = Manager("..")
-beautify(newNamePrefix, start, step)
+beautify(start=start, step=step, newNamePrefix=newNamePrefix)
 
 
