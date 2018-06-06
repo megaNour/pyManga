f076064935b63e5093c46035b1f2dc38b99957d1
nour al kotob <nour.alkotob@ge.com>
2018-06-06 15:42:51 +0200

    scribus calls like a boss

--- scribus2.py
+++ scribus2.py
@@ -10,6 +10,7 @@ import argparse
 #import shutil
 from pathlib import Path
 import re
+import sys
 
 manager = Manager("..")
 name = manager.getChapterName()
@@ -25,10 +26,19 @@ args, unknown = parser.parse_known_args()
 
 pagesArgs = ""
 if args.p:
-    pagesArgs = " -p " + ",".join(args.p)
+    pagesArgs = "p:" + ",".join(args.p)
     pagesArgs = constants.zfillParamString(pagesArgs, 2)
 
 parent = os.getcwd()
 
-printAndRun(scribus + " -g -ns -py " + scribusScriptsPath  + "pdf2.py"  
-+ " -n " + name + " -d " + parent + pagesArgs) 
+sys.argv.append("-s")
+sys.argv.append("pdf2.py")
+
+sys.argv.append("-n")
+sys.argv.append(name)
+
+if args.p:
+	sys.argv.append("-a")
+	sys.argv.append(pagesArgs)
+
+import scribus3
--- scribus3.py
+++ scribus3.py
@@ -18,22 +18,26 @@ scribusScriptsPath = os.environ["SCRIBUSSCRIPTSPATH"] + "/"
 scribus = "scribusNour.AppImage" if os.name !="nt" else "\"" + os.environ["SCRIBUSPATH"] + "/Scribus.exe" + "\""
 pages = ""
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", required=True, nargs="*", help="the script to run from the scribusScriptsPath")
+parser.add_argument("-s", required=True, help="the script to run from the scribusScriptsPath")
 parser.add_argument("-n", required=True, help="the name of the sla file to work with")
-parser.add_argument("-a", help="additional arguments")
+parser.add_argument("-a", nargs="*", help="additional arguments for -p you do p:1,2,3,4")
 
 args, unknown = parser.parse_known_args()
 #args.D is defined in Webtoonify.py
 
+parent = os.getcwd()
+
 additionalArgs = ""
 if args.a:
-	additionalArgs = args.ab
-
-if args.p:
-    pagesArgs = " -p " + ",".join(args.p)
-    pagesArgs = constants.zfillParamString(pagesArgs, 2)
-
-parent = os.getcwd()
+	for argString in args.a:
+		split = argString.split(":")
+		flag = "-" + split[0]
+		arguments = " ".join(split[1].split(","))
+		argument = " " + flag + " " + arguments
+		print("injecting argument: " + argument)
+		additionalArgs += argument 
+		additionalArgs = additionalArgs.rstrip()
+		print(additionalArgs)
 
 printAndRun(scribus + " -g -ns -py " + scribusScriptsPath  + args.s  
 + " -n " + args.n + " -d " + parent + additionalArgs) 
